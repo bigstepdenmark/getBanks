1464bd35c32ead4de439fdcc51d1397374154ffc
Mazlum <mazlumsert@gmail.com>
2017-10-03 17:57:48 +0200

    add controller and method

---
+++ src/main/java/dk/cphsoftdev/app/controller/BankController.java
@@ -0,0 +1,17 @@
+package dk.cphsoftdev.app.controller;
+import dk.cphsoftdev.app.entity.Bank;
+
+public class BankController {
+
+    /**
+     * This method has the job of sending info to the recipient Application
+     * @param bank Bank
+     * @return bank
+     */
+    public Bank sendBankInfo(Bank bank){ return bank; }
+    
+
+
+
+
+}
--- src/main/java/dk/cphsoftdev/app/entity/Bank.java
+++ src/main/java/dk/cphsoftdev/app/entity/Bank.java
@@ -1,6 +1,47 @@
 package dk.cphsoftdev.app.entity;
 
-public class Bank {
 
-    
+public class Bank {
+   private int creditScore;
+   private int minCreditScore;
+   private String bankName;
+
+    public Bank(int creditScore, int minCreditScore, String bankName) {
+        this.creditScore = creditScore;
+        this.minCreditScore = minCreditScore;
+        this.bankName = bankName;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public int getMinCreditScore() {
+        return minCreditScore;
+    }
+
+    public void setMinCreditScore(int minCreditScore) {
+        this.minCreditScore = minCreditScore;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "creditScore=" + creditScore +
+                ", minCreditScore=" + minCreditScore +
+                ", bankName='" + bankName + '\'' +
+                '}';
+    }
 }
