3edb67823560104ca11cef5b6e68ab4b2ed0a265
Mazlum <mazlumsert@gmail.com>
2017-11-16 11:53:25 +0100

    skerder

--- src/main/java/dk/cphsoftdev/app/Main.java
+++ src/main/java/dk/cphsoftdev/app/Main.java
@@ -6,22 +6,19 @@ import dk.cphsoftdev.app.controller.SenderController;
 
 /**
  * Hello world!
- *
  */
 public class Main
 {
     public static void main( String[] args ) {
-    //    ReceiverController receiver = new ReceiverController("group3.creditscore.getbanks1","datdb.cphbusiness.dk","guest");
+        ReceiverController receiver = new ReceiverController("group3.creditscore.getbanks1","datdb.cphbusiness.dk","guest");
         SenderController send = new SenderController("guest","datdb.cphbusiness.dk","group3.getbanks.reciplist");
 
-       // if(receiver.isReady()){
-             //  for (String msg: receiver.getMessages()) {
-
-                        send.SendMSG("sker der joe moe");
-              //  }
-
+        if(receiver.isReady()){
+               for (String msg: receiver.getMessages()) {
+                        send.SendMSG(msg);
+                }
                         send.close();
-      // receiver.close();
-       // }
+                        receiver.close();
+        }
     }
 }
