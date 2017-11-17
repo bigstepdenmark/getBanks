118d6432fed0188f7032356af3873154b90a3761
Ismail Cam <i@bigstep.dk>
2017-11-17 20:31:27 +0100

    ide conf

--- src/main/java/dk/cphsoftdev/app/Main.java
+++ src/main/java/dk/cphsoftdev/app/Main.java
@@ -4,21 +4,21 @@ import dk.cphsoftdev.app.controller.ReceiverController;
 import dk.cphsoftdev.app.controller.SenderController;
 
 
-/**
- * Hello world!
- */
 public class Main
 {
-    public static void main( String[] args ) {
-        ReceiverController receiver = new ReceiverController("group3.creditscore.getbanks1","datdb.cphbusiness.dk","guest");
-        SenderController send = new SenderController("guest","datdb.cphbusiness.dk","group3.getbanks.reciplist");
+    public static void main( String[] args )
+    {
+        ReceiverController receiver = new ReceiverController( "group3.creditscore.getbanks1", "datdb.cphbusiness.dk", "guest" );
+        SenderController send = new SenderController( "guest", "datdb.cphbusiness.dk", "group3.getbanks.reciplist" );
 
-        if(receiver.isReady()){
-               for (String msg: receiver.getMessages()) {
-                        send.SendMSG(msg);
-                }
-                        send.close();
-                        receiver.close();
+        if( receiver.isReady() )
+        {
+            for( String msg : receiver.getMessages() )
+            {
+                send.SendMSG( msg );
+            }
+            send.close();
+            receiver.close();
         }
     }
 }
